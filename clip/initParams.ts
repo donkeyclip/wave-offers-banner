f4ae862f3e4966f93ce2e35709c0f48538491a51
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2023-05-23 10:15:54 +0300

    fix: fix urls

--- clip/initParams.ts
+++ clip/initParams.ts
@@ -5,15 +5,15 @@ export default [
       backgroundLinearColor1:"#6AC0FF",
       backgroundLinearColor2:"#0073FA",
       fontColor:"#fff",
-      wave1:"assets/w1.png",
-      wave2:"assets/w2.png",
-      wave3:"assets/w3.png",
+      wave1:"https://donkeyclip.github.io/wave-offers-clip/server/assets/w1.png",
+      wave2:"https://donkeyclip.github.io/wave-offers-clip/server/assets/w2.png",
+      wave3:"https://donkeyclip.github.io/wave-offers-clip/server/assets/w3.png",
       text:"summer offers",
       offer:"50 % off",
       products:[
-       { image:"assets/pr1.png"},
-       { image:"assets/pr2.png"},
-       { image:"assets/pr3.png"}
+       { image:"https://donkeyclip.github.io/wave-offers-clip/server/assets/pr1.png"},
+       { image:"https://donkeyclip.github.io/wave-offers-clip/server/assets/pr2.png"},
+       { image:"https://donkeyclip.github.io/wave-offers-clip/server/assets/pr3.png"}
        ]
     }
   },
